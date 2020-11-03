3391547775677a6de28064f8685a78bac658a076
Azat Davliatshin <ardavlyatshin@gmail.com>
2020-11-03 17:46:30 +0300

    Task 3 Product API: - add unit tests for getProductsList

--- product-service/handlers/getProductsList/getProductsList.ts
+++ product-service/handlers/getProductsList/getProductsList.ts
@@ -1,6 +1,6 @@
 import { APIGatewayProxyHandler } from "aws-lambda";
 import 'source-map-support/register';
-import * as api from '../api';
+import * as api from '../../api';
 
 export const getProductsList: APIGatewayProxyHandler = async () => {
     const products = await api.getProductList();
