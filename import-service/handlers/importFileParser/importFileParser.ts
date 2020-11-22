f7688d72c03f80be9f21ea7c6d252640ec13b0aa
Azat Davliatshin <ardavlyatshin@gmail.com>
2020-11-22 23:51:57 +0300

    Task 5 RDS: - move parsed file to "parsed" folder;

--- import-service/handlers/importFileParser/importFileParser.ts
+++ import-service/handlers/importFileParser/importFileParser.ts
@@ -2,17 +2,25 @@ import { S3Handler } from "aws-lambda";
 import {S3} from 'aws-sdk';
 import * as csv from 'csv-parser';
 
-export const importFileParser: S3Handler = async event => {
+export const importFileParser: S3Handler = event => {
     const s3 = new S3({region: 'eu-west-1', signatureVersion: 'v4'});
 
     for (const record of event.Records) {
         try {
-            s3.getObject({Bucket: record.s3.bucket.name, Key: record.s3.object.key}).createReadStream()
+            const {bucket: {name: Bucket}, object: {key: Key}} = record.s3;
+            s3.getObject({Bucket, Key}).createReadStream()
                 .on('error', error => console.log(`Service error: ${error}`))
                     .pipe(csv())
                     .on('error', error => console.log(error))
                     .on('data', chunk => console.log(JSON.stringify(chunk)))
-                    .on('close', () => console.log('COPY'));
+                    .on('end', async () => {
+                        await s3.copyObject({
+                            Bucket,
+                            CopySource: `${Bucket}/${Key}`,
+                            Key: Key.replace('uploaded', 'parsed')
+                        }).promise();
+                        await s3.deleteObject({Bucket, Key}).promise();
+                    });
         } catch(exception) {
             console.log(exception);
         }
